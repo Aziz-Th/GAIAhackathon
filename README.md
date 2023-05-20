8ba53fdb4602f682e508ee30b9bf5dff803a26b0
Abdulaziz <abdulaziza.althumairy@gmail.com>
2023-05-21 00:32:33 +0300

    deployy wth gpt

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# GAIAhackathon
+# GAIAhackathon 
+
+# Summarizer -a project by LEVEL 8
+
+this project takes advantage of powerful generative AI models such as ..... to help us, make long content(podcasts, interviews) more accessable, and give the listeners a brief description before they spend 2.5-3.0 hours on content.
+
+for security issues and because we are using fast solutions like streamlit, please provide your OpenAI API key, to try the application.
+
+NOTE: THIS IS A PROTOTYPE AND FOR DEMONSTRATION PURPOSES ONLY, AND WE WON'T ASK FOR AN API KEY IN THE FINAL PRODUCT!!
--- generate.py
+++ generate.py
@@ -15,7 +15,7 @@ co = cohere.Client(CO_API_KEY) # This is your trial API key
 
 
 sumsum=''
-def summerize(paragraph,creativity):
+def summerize(paragraph,creativity,language):
     if '=' in paragraph:
         paragraph=paragraph.split('=')
         paragraph = (paragraph[1])
@@ -40,15 +40,17 @@ def summerize(paragraph,creativity):
             length='long',
             format='paragraph',
             model='summarize-xlarge',
-            additional_command='write it as a preview, very general, write it as a description',
+            additional_command='write it as a preview, very general, write it as a description, write it in third person',
             # additional_command='Write an introductory paragraph for a blog post about language models.',
 
             temperature=creativity/10,
             extractiveness='auto',)
         summery = response.summary
+        if language=='Arabic':
+            translator = Translator()
+            translated = translator.translate(summery, src='en', dest='ar')
+            summery=translated.text
 
-        # translator = Translator()
-        # translated = translator.translate(summery, src='en', dest='ar')
 
         return (summery)
 
@@ -63,15 +65,17 @@ def summerize(paragraph,creativity):
 
 
 
+langs=['Arabic','English']
 st.title("Podcast Summarizer")
 form = st.form(key="user_settings")
 with form:
     para_input = st.text_input("URL", key = "link_input")
+    # api_input = st.text_input("API KEY", key = "api_input")
     Creativity = st.slider('Creativity',min_value=1,max_value=10,value=3,help='Indicates The Randomness of The Summarization',)
+    lang = st.selectbox('Language',options=langs)
     generate_button = form.form_submit_button("Summarize")
     if generate_button:
-        sumsum = summerize(para_input,Creativity)
+        sumsum = summerize(para_input,Creativity,lang)
         st.write(sumsum)
-
-    st.components.v1.html((f'<a href="https://twitter.com/intent/tweet?text={sumsum}" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'), scrolling=False)
+    # st.components.v1.html((f'<a href="https://twitter.com/intent/tweet?text={sumsum}" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'), scrolling=False)
 
