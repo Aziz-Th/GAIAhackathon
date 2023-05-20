84f0a80f34377d476a2970d9dc681236e1408e5b
Abdulaziz <abdulaziza.althumairy@gmail.com>
2023-05-20 22:20:57 +0300

    deployy wth gpt

---
+++ chatgpt.py
@@ -0,0 +1,79 @@
+# This Python file uses the following encoding: utf-8
+import cohere
+from googletrans import Translator
+import streamlit as st
+from youtube_transcript_api import YouTubeTranscriptApi
+import openai
+import os
+
+import pandas as pd
+
+import time
+
+openai.api_key = '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'
+
+co = cohere.Client('PlUWaTH2F6AkPZ3YpD0jLE7CkWsRcRClMdnvHcHh') # This is your trial API key
+
+
+transcript = YouTubeTranscriptApi.get_transcript('pJ0auP7dbcY&t')
+txtlist=[]
+for i in transcript:
+    outtxt = (i['text'])
+    txtlist.append(outtxt)
+
+txtlist = ' '.join(txtlist)
+text = txtlist[:3000]
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+
+    messages = [{"role": "user", "content": prompt}]
+
+    response = openai.ChatCompletion.create(
+
+    model='gpt-3.5-turbo',
+
+    messages=messages,
+
+    temperature=0,
+
+    )
+
+    return response.choices[0].message["content"]
+
+# prompt = f"You are tasked with summerizing what the following podcast content is about briefly, Please summarize the following text in arabic,:{text}"
+
+prompt = f"you are tasked with summarizing a segment in the following podcast, Please summarize the following text in arabic briefly,:{text}"
+
+
+response = get_completion(prompt)
+
+print(response)
+# def summerize(paragraph):
+#   brief = co.summarize(
+#     text=paragraph,
+#     length='short',
+#     format='paragraph',
+#     extractiveness='low',
+#     model='summarize-xlarge',
+#     additional_command='',
+#     temperature=0.3,)
+#   translator = Translator()
+#   translated = translator.translate(brief, src='en', dest='ar')
+#   return translated.text
+
+# st.title("🚀 Startup Idea Generator")
+# form = st.form(key="user_settings")
+# with form:
+#     para_input = st.text_input("text", key = "text_input")
+#     generate_button = form.form_submit_button("summerize")
+#     if generate_button:
+#         sumsum = summerize(para_input)
+#         st.write(sumsum)
+
+
+# summery = response.summary
+#
+# translator = Translator()
+# translated = translator.translate(summery,src='en',dest='ar')
+# print(translated.text)
+
--- chatgptlong.py
+++ chatgptlong.py
@@ -1,21 +1,23 @@
 # py -m pip install -r requirements.txt
 
 # This Python file uses the following encoding: utf-8
-import cohere
 from googletrans import Translator
 import streamlit as st
 import streamlit.components.v1 as components
 
 from youtube_transcript_api import YouTubeTranscriptApi
 
-CO_API_KEY='PlUWaTH2F6AkPZ3YpD0jLE7CkWsRcRClMdnvHcHh'
-co = cohere.Client(CO_API_KEY) # This is your trial API key
+import openai
+import os
 
+import pandas as pd
 
+import time
 
+openai.api_key = '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'
 
 sumsum=''
-def summerize(paragraph,creativity):
+def summerize(paragraph,creativity,language):
     if '=' in paragraph:
         paragraph=paragraph.split('=')
         paragraph = (paragraph[1])
@@ -35,22 +37,38 @@ def summerize(paragraph,creativity):
         #     text = txtlist[t:t+10000]
         text=txtlist[:10000]
 
-        response = co.summarize(
-            text=text,
-            length='long',
-            format='paragraph',
-            model='summarize-xlarge',
-            additional_command='write it as a preview, very general, write it as a description',
-            # additional_command='Write an introductory paragraph for a blog post about language models.',
 
-            temperature=creativity/10,
-            extractiveness='auto',)
-        summery = response.summary
 
         # translator = Translator()
         # translated = translator.translate(summery, src='en', dest='ar')
 
-        return (summery)
+        def get_completion(prompt, model="gpt-3.5-turbo"):
+
+            messages = [{"role": "user", "content": prompt}]
+
+            response = openai.ChatCompletion.create(
+
+                model='gpt-3.5-turbo',
+
+                messages=messages,
+
+                temperature=0,
+
+            )
+
+            return response.choices[0].message["content"]
+
+        # prompt = f"You are tasked with summerizing what the following podcast content is about briefly, Please summarize the following text in arabic,:{text}"
+
+        if language == 'Arabic':
+            lang='arabic'
+        else:
+            lang='english'
+
+        prompt = f"you are tasked with summarizing a segment in the following podcast, Please summarize the following text in {lang} briefly,:{text}"
+
+        response = get_completion(prompt)
+        return response
 
 
 
@@ -62,15 +80,16 @@ def summerize(paragraph,creativity):
 
 
 
-
+langs=['Arabic','English']
 st.title("Podcast Summarizer")
 form = st.form(key="user_settings")
 with form:
     para_input = st.text_input("URL", key = "link_input")
     Creativity = st.slider('Creativity',min_value=1,max_value=10,value=3,help='Indicates The Randomness of The Summarization',)
+    lang = st.selectbox('Language',options=langs)
     generate_button = form.form_submit_button("Summarize")
     if generate_button:
-        sumsum = summerize(para_input,Creativity)
+        sumsum = summerize(para_input,Creativity,lang)
         st.write(sumsum)
 
     st.components.v1.html((f'<a href="https://twitter.com/intent/tweet?text={sumsum}" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'), scrolling=False)
